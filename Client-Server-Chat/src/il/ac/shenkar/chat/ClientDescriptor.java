316564cf29c43408effacb338edd9cb34ba9a02a
Ofir <ofir.duchovne@gmail.com>
2021-11-27 16:39:51 +0200

    client descriptor

---
+++ Client-Server-Chat/src/il/ac/shenkar/chat/ClientDescriptor.java
@@ -0,0 +1,22 @@
+package il.ac.shenkar.chat;
+
+public class ClientDescriptor implements StringConsumer, StringProducer {
+
+    private MessageBoard mb;
+
+
+    @Override
+    public void consume(String str) {
+        mb.consume(str);
+    }
+
+    @Override
+    public void addConsumer(StringConsumer sc) {
+        mb = (MessageBoard) sc;
+    }
+
+    @Override
+    public void removeConsumer(StringConsumer sc) {
+        mb = null;
+    }
+}
